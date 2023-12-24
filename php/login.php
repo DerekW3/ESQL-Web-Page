37db2ed94271c86aa9efbc910de282f197de3cdf
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-24 13:35:21 +0100

    Build: crea login.php e rinominare styles.css per espandabilita

---
+++ php/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+</head>
+
+<body>
+    <?php
+        $nome=$_POST["nomeUtente"];
+        $cognome=$_POST["cognomeUtente"];
+        $email=$_POST["emailUtente"];
+
+        echo $nome;
+        echo $cognome;
+        echo $email;
+    ?>
+</body>
