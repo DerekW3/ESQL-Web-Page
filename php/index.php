909675a4de89416e8af65a5991ebd7cceae1b508
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-24 18:04:50 +0100

    Refactor: cambia il nome del file php

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+</head>
+
+<body>
+    <?php
+        $nome=$_POST["nomeUtente"];
+        $cognome=$_POST["cognomeUtente"];
+        $email=$_POST["emailUtente"];
+    ?>
+</body>
