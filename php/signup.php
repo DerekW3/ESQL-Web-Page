afb44af1d9aa7d484385ea8f7edc38e659f3e6c6
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-28 03:57:29 +0100

    Build: Build database connection in signup.php

--- php/signup.php
+++ php/signup.php
@@ -9,5 +9,15 @@
         $nome=$_POST["nomeUtente"];
         $cognome=$_POST["cognomeUtente"];
         $email=$_POST["emailUtente"];
+
+        try {
+            $pdo=new PDO("mysql:host=localhost; dbname=ESQL", "root", "secretpassword1");
+            
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec('SET NAMES "utf8"');
+        } catch(PDOException) {
+            echo("Connessione non riuscita");
+            exit();
+        }
     ?>
 </body>
