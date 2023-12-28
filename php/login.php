988e770536fff491cbd7346eb65c7e60173be918
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-28 03:55:39 +0100

    Build: Build MySQL database connection and refactor codebase to be more readable

--- php/login.php
+++ php/login.php
@@ -9,5 +9,23 @@
         $nome=$_POST["nomeUtente"];
         $cognome=$_POST["cognomeUtente"];
         $email=$_POST["emailUtente"];
+
+        try {
+            $pdo=new PDO("mysql:host=localhost; dbname=ESQL", "root", "secretpassword1");
+            
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec('SET NAMES "utf8"');
+        } catch(PDOException) {
+            echo("Connessione non riuscita");
+            exit();
+        }
+
+        try {
+            $sql ='SELECT * FROM UTENTI WHERE $nome=Nome, $cognome=Cognome, $email=Email';
+            $result=$pdo->query($sql);
+        } catch (PDOException) {
+            header("Location: ../webpages/signup.html");
+            exit();
+        }
     ?>
 </body>
