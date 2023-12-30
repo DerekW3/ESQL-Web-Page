f0d50437e60a98997497b17888baf50fe4b453d3
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-30 18:26:33 +0100

    Build: Add cookies and  variables

--- php/login.php
+++ php/login.php
@@ -7,7 +7,7 @@
 <body>
     <?php
         $email=$_POST["emailUtente"];
-        $email=$_POST["password"];
+        $password=$_POST["password"];
 
         try {
             $pdo=new PDO("mysql:host=localhost; dbname=ESQL", "root", "secretpassword1");
@@ -20,12 +20,21 @@
         }
 
         try {
-            $sql ="SELECT USER FROM mysql.user WHERE USER = '$email'";
+            $sql="SELECT * FROM UTENTI WHERE Email = '$email'";
             $result=$pdo->query($sql);
             if ($result->rowCount() == 0) {
                 header("Location: ../webpages/select-type.html");
                 exit();
             } else {
+                foreach($result as $row) {
+                    $name=$row['Nome'];
+                    $cognome=$row['Cognome'];
+                }
+                setcookie("name", $name, time() + 3.6e6);
+                setcookie("cognome", $cognome, time() + 3.6e6);
+
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
                 // TODO: Redirect to the main page based on type
             }
         } catch (PDOException) {
