1f01e6e2cd6c7a192d65189a5de1a09af41be395
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-02 15:23:52 +0100

    Build: Access MySQL database with specific user

--- php/login.php
+++ php/login.php
@@ -14,41 +14,37 @@ session_start();
     $password = $_POST["password"];
 
     try {
-        $pdo = new PDO("mysql:host=localhost; dbname=ESQL", "ESQLadmin", "esqladminpassword1");
+        $pdo = new PDO("mysql:host=localhost; dbname=ESQL", $email, $password);
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->exec('SET NAMES "utf8"');
-    } catch (PDOException) {
-        echo ("Connessione non riuscita");
+    } catch (PDOException $e) {
+        echo ("Connessione non riuscita") . $e->getMessage();
+        header("Location: ../webpages/select-type.html");
         exit();
     }
 
     try {
         $sql = "SELECT * FROM UTENTI WHERE Email = '$email'";
         $result = $pdo->query($sql);
-        if ($result->rowCount() == 0) {
-            header("Location: ../webpages/select-type.html");
+        foreach ($result as $row) {
+            $name = $row['Nome'];
+            $cognome = $row['Cognome'];
+        }
+        setcookie("name", $name, time() + 3.6e6);
+        setcookie("cognome", $cognome, time() + 3.6e6);
+
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+
+        $sql = "SELECT * FROM STUDENTI WHERE EmailUtente = '$email'";
+        $result = $pdo->query($sql);
+        if ($result->rowCount() > 0) {
+            header("Location: ./student-homepage.php");
             exit();
         } else {
-            foreach ($result as $row) {
-                $name = $row['Nome'];
-                $cognome = $row['Cognome'];
-            }
-            setcookie("name", $name, time() + 3.6e6);
-            setcookie("cognome", $cognome, time() + 3.6e6);
-
-            $_SESSION['email'] = $email;
-            $_SESSION['password'] = $password;
-
-            $sql = "SELECT * FROM STUDENTI WHERE EmailUtente = '$email'";
-            $result = $pdo->query($sql);
-            if ($result->rowCount() > 0) {
-                header("Location: ./student-homepage.php");
-                exit();
-            } else {
-                header("Location: ./professor-homepage.php");
-                exit();
-            }
+            header("Location: ./professor-homepage.php");
+            exit();
         }
     } catch (PDOException) {
         exit();
--- php/signup-docente.php
+++ php/signup-docente.php
@@ -1,63 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-    </head>
+
+<head>
+</head>
 </head>
 
 <body>
     <?php
-        $nome=$_POST["nomeUtente"];
-        $cognome=$_POST["cognomeUtente"];
-        $email=$_POST["emailUtente"];
-        $telefono=$_POST["telefonoUtente"];
-        $dipartimento=$_POST["nomeDipartimento"];
-        $corso=$_POST["nomeCorso"];
-        $password=md5($_POST["password"]);
+    $nome = $_POST["nomeUtente"];
+    $cognome = $_POST["cognomeUtente"];
+    $email = $_POST["emailUtente"];
+    $telefono = $_POST["telefonoUtente"];
+    $dipartimento = $_POST["nomeDipartimento"];
+    $corso = $_POST["nomeCorso"];
+    $password = $_POST["password"];
 
-        try {
-            $pdo=new PDO("mysql:host=localhost; dbname=ESQL", "ESQLadmin", "esqladminpassword1");
-            
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->setAttribute(PDO::ATTR_AUTOCOMMIT, FALSE);
-            $pdo->exec('SET NAMES "utf8"');
-        } catch(PDOException $e) {
-            echo("Connessione non riuscita").$e->getMessage();
+    try {
+        $pdo = new PDO("mysql:host=localhost; dbname=ESQL", "ESQLadmin", "esqladminpassword1");
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_AUTOCOMMIT, FALSE);
+        $pdo->exec('SET NAMES "utf8"');
+    } catch (PDOException $e) {
+        echo ("Connessione non riuscita") . $e->getMessage();
+        exit();
+    }
+
+    try {
+        $sql = "SELECT USER FROM mysql.user WHERE USER = '$email'";
+        $result = $pdo->query($sql);
+        if ($result->rowCount() > 0) {
+            header("Location: ../index.html");
             exit();
         }
+    } catch (PDOException) {
+    }
 
-        try {
-            $sql ="SELECT USER FROM mysql.user WHERE USER = '$email'";
-            $result=$pdo->query($sql);
-            if ($result->rowCount() > 0) {
-                header("Location: ../index.html");
-                exit();
-            }
-        } catch (PDOException) { }
+    try {
+        $sql = "CREATE USER '$email'@'localhost' IDENTIFIED BY '$password'";
+        $result = $pdo->query($sql);
 
-        try {
-            $sql="CREATE USER '$email'@'localhost' IDENTIFIED BY '$password'";
-            $result=$pdo->query($sql);
+        $sql = "GRANT SELECT, INSERT, UPDATE on ESQL.* TO '$email'@'localhost'";
+        $result = $pdo->query($sql);
 
-            $sql="GRANT SELECT, INSERT, UPDATE on ESQL.* TO '$email'@'localhost'";
-            $result=$pdo->query($sql);
-
-            if (!empty($telefono)) {
-                $numeroTelefono = $telefono;
-            } else {
-                $numeroTelefono = 0;
-            }
-
-            $sql="CALL iscrivi_docente('$email', '$nome', '$cognome', '$numeroTelefono', '$dipartimento', '$corso')";
-            $result=$pdo->exec($sql);
-
-            $pdo->commit();
-        } catch (PDOException $e) {
-            echo('Codice errore'.$e->getMessage());
-            echo('Signup Failed');
-            $pdo->rollBack();
+        if (!empty($telefono)) {
+            $numeroTelefono = $telefono;
+        } else {
+            $numeroTelefono = 0;
         }
 
-        header("Location: ../index.html");
-        exit();
+        $sql = "CALL iscrivi_docente('$email', '$nome', '$cognome', '$numeroTelefono', '$dipartimento', '$corso')";
+        $result = $pdo->exec($sql);
+
+        $pdo->commit();
+    } catch (PDOException $e) {
+        echo ('Codice errore' . $e->getMessage());
+        echo ('Signup Failed');
+        $pdo->rollBack();
+    }
+
+    header("Location: ../index.html");
+    exit();
     ?>
 </body>
