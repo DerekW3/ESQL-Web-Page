853eaed9c9b2119b1b4a5d1f99720416c9f4ee88
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-02 19:29:47 +0100

    Build: Build professor homepage boilerplate

---
+++ php/professor-homepage.php
@@ -0,0 +1,19 @@
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="../styles/professor-homepage.css">
+</head>
+
+<body>
+    <div class="header">
+        <h3><?php echo ("Ciao, " . $_COOKIE['name']); ?></h3>
+        <a href="./messaggi.php"><img id="messaggi" src="https://cdn3.iconfinder.com/data/icons/email-51/48/53-512.png" alt="Simbolo per messaggi"></a>
+    </div>
+</body>
