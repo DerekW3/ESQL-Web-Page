37c21572bfd654c1444b361c8f06708c7b10eb23
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-02 19:39:48 +0100

    Style: Recolor text

--- php/professor-homepage.php
+++ php/professor-homepage.php
@@ -17,6 +17,36 @@ session_start()
         <a href="./messaggi.php"><img id="messaggi" src="https://cdn3.iconfinder.com/data/icons/email-51/48/53-512.png" alt="Simbolo per messaggi"></a>
     </div>
     <div class="content">
-        
+        <h3 id="testTitle">Test Disponibili<h3>
+                <div class="tests">
+                    <?php
+                    try {
+                        $pdo = new PDO("mysql:host=localhost; dbname=ESQL", $_SESSION['email'], $_SESSION['password']);
+
+                        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $pdo->exec('SET NAMES "utf8"');
+                    } catch (PDOException $e) {
+                        echo ("Connessione non riuscita") . $e->getMessage();
+                        exit();
+                    }
+
+                    try {
+                        $sql = "SELECT * FROM TEST";
+                        $result = $pdo->query($sql);
+
+                        if ($result->rowCount() == 0) {
+                            echo '<div id="none">Nessun Test Disponibile</div>';
+                        } else {
+                            foreach ($result as $row) {
+                                $name = $row['Nome'];
+                                $cognome = $row['Cognome'];
+                            }
+                        }
+                    } catch (PDOException $e) {
+                        echo ("Query Fallito") . $e->getMessage();
+                        exit();
+                    }
+                    ?>
+                </div>
     </div>
 </body>
