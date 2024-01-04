2faff7b20da11d6121cbacba10c18f585f4b83a0
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-04 21:59:42 +0100

    Build: Build boilerplate for create-question.php

--- php/create-question.php
+++ php/create-question.php
@@ -1,11 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <?php
+    $titoloTest = $_POST['titoloTest'];
+    $descrizione = $_POST['descrizione'];
+    $livelloDifficolta = $_POST['livelloDifficolta'];
+    $tipoQuesito = $_POST['tipoQuesito'];
+
+    try {
+        $pdo = new PDO("mysql:host=localhost; dbname=ESQL", $_SESSION['email'], $_SESSION['password']);
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec('SET NAMES "utf8"');
+    } catch (PDOException $e) {
+        echo ("Connessione non riuscita") . $e->getMessage();
+        exit();
+    }
+    ?>
 </body>
+
 </html>
