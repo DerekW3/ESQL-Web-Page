00faf5dc63d377824494e7c3ab5024f6cb1e6e68
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-17 13:44:24 -0600

    Build: Build framework for answering questions

---
+++ php/question-response.php
@@ -0,0 +1,92 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rispondi</title>
+    <link rel="stylesheet" href="../styles/signup.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        $numeroQuesito = $_POST['numeroQuesito'];
+        $titoloTest = $_POST['titoloTest'];
+        $testNum = $_POST['page'];
+
+        if (isset($_COOKIE['page'])) {
+            $testNum = $_COOKIE['page'];
+        } else {
+            $testNum = $_POST['page'];
+            setcookie('page', $testNum, time() + 3.6e6);
+        }
+
+        $email = $_SESSION['email'];
+
+        try {
+            $pdo = new PDO("mysql:host=localhost; dbname=ESQL", $_SESSION['email'], $_SESSION['password']);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec('SET NAMES "utf8"');
+        } catch (PDOException $e) {
+            echo ("Connessione non riuscita") . $e->getMessage();
+            exit();
+        }
+
+        try {
+            $sql = "SELECT * FROM QUESITI WHERE TitoloTest LIKE '$titoloTest' AND Numero = '$numeroQuesito'";
+            $result = $pdo->query($sql);
+
+            $tipoQuesito = 0;
+
+            $sql2 = "SELECT * FROM CODICE WHERE TitoloTest LIKE '$titoloTest' AND NumeroQuesito = '$numeroQuesito'";
+            $result2 = $pdo->query($sql2);
+
+            if ($result2->rowCount() == 0) {
+                $tipoQuesito = 1;
+                $sql2 = "SELECT * FROM RISPOSTA_CHIUSA WHERE TitoloTest LIKE '$titoloTest' AND NumeroQuesito = '$numeroQuesito'";
+                $result2 = $pdo->query($sql2);
+            }
+        } catch (PDOException $e) {
+            echo ("Fallito") . $e->getMessage();
+            exit();
+        }
+        ?>
+        <form action="./check-answer.php" method="post" id="answerQuestion">
+            <div class="container-three">
+                <?php
+                try {
+                    if ($tipoQuesito == 0) {
+                        foreach ($result as $row) {
+                            $descrizione = $row['Descrizione'];
+                        }
+                        echo
+                        "<label for=\"soluzione\">$descrizione</label>
+                        <textarea id=\"soluzione\" name=\"soluzione\" rows=\"5\" cols=\"80\" placeholder=\"Inserisci Codice\"></textarea>";
+                    } else {
+                        $index = 1;
+                        foreach ($result as $row) {
+                            $descrizione = $row['Descrizione'];
+                            echo
+                            "<input type=\"radio\" id=\"$index\" name=\"$index\" value=\"$index\" required>
+                            <label for=\"$index\">$descrizione</label>";
+                            $index += 1;
+                        }
+                    }
+                } catch (PDOException $e) {
+                    echo ("Fallito") . $e->getMessage();
+                    exit();
+                }
+                ?>
+            </div>
+        </form>
+
+        <button style="margin-top: 10px;" type="submit" form="answerQuestion" value="Submit">Rispondi</button>
+    </div>
+</body>
+
+</html>
