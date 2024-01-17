5b51c247bd93142df19310cf16e572e9e4ea76de
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-17 17:57:11 -0600

    Build: Add table displaying

--- php/professor-homepage.php
+++ php/professor-homepage.php
@@ -17,41 +17,44 @@ session_start()
         <a href="./messaggi.php"><img id="messaggi" src="https://cdn3.iconfinder.com/data/icons/email-51/48/53-512.png" alt="Simbolo per messaggi"></a>
     </div>
     <div class="content">
-        <h3 id="title">Test Disponibili<h3>
-                <div class="tests">
-                    <?php
-                    if (isset($_COOKIE['titoloTest'])) {
-                        setcookie('titoloTest', "", time() - 3600);
-                    }
-                    if (isset($_COOKIE['numQuesito'])) {
-                        setcookie('numQuesito', "", time() - 3600);
-                    }
-                    if (isset($_COOKIE['page'])) {
-                        setcookie('page', "", time() - 3600);
-                    }
-                    try {
-                        $pdo = new PDO("mysql:host=localhost; dbname=ESQL", $_SESSION['email'], $_SESSION['password']);
+        <h3 id="title">Test Disponibili</h3>
+        <div class="tests">
+            <?php
+            if (isset($_COOKIE['titoloTest'])) {
+                setcookie('titoloTest', "", time() - 3600);
+            }
+            if (isset($_COOKIE['numQuesito'])) {
+                setcookie('numQuesito', "", time() - 3600);
+            }
+            if (isset($_COOKIE['page'])) {
+                setcookie('page', "", time() - 3600);
+            }
 
-                        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                        $pdo->exec('SET NAMES "utf8"');
-                    } catch (PDOException $e) {
-                        echo ("Connessione non riuscita") . $e->getMessage();
-                        exit();
-                    }
+            $email = $_SESSION['email'];
 
-                    try {
-                        $sql = "SELECT * FROM TEST ORDER BY DataCreazione DESC";
-                        $result = $pdo->query($sql);
+            try {
+                $pdo = new PDO("mysql:host=localhost; dbname=ESQL", $_SESSION['email'], $_SESSION['password']);
 
-                        if ($result->rowCount() == 0) {
-                            echo '<div id="none">Nessun Test Disponibile</div>';
-                        } else {
-                            $testNum = 1;
-                            foreach ($result as $row) {
-                                $titolo = $row['Titolo'];
-                                $dataCreazione = $row['DataCreazione'];
-                                echo
-                                "<div class=\"test\">
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $pdo->exec('SET NAMES "utf8"');
+            } catch (PDOException $e) {
+                echo ("Connessione non riuscita") . $e->getMessage();
+                exit();
+            }
+
+            try {
+                $sql = "SELECT * FROM TEST ORDER BY DataCreazione DESC";
+                $result = $pdo->query($sql);
+
+                if ($result->rowCount() == 0) {
+                    echo '<div id="none">Nessun Test Disponibile</div>';
+                } else {
+                    $testNum = 1;
+                    foreach ($result as $row) {
+                        $titolo = $row['Titolo'];
+                        $dataCreazione = $row['DataCreazione'];
+                        echo
+                        "<div class=\"test\">
                                     <div id=\"info\">
                                         <h3 style=\"color: var(--text);\">$titolo</h3>
                                         <h3 style=\"color: var(--text);\">$dataCreazione</h3>
@@ -62,19 +65,48 @@ session_start()
                                         <button type=\"submit\">Accedi</button>
                                     </form>
                                 </div>";
-                                $testNum += 1;
-                            }
-                        }
-                    } catch (PDOException $e) {
-                        echo ("Query Fallito") . $e->getMessage();
-                        exit();
+                        $testNum += 1;
                     }
-                    ?>
-                </div>
-                <h3 id="title">Azioni Disponibili</h3>
-                <div class="azioni">
-                    <a href="../webpages/create-table-esercizio.html"><button class="azione">Crea Tabella Esercizio</button></a>
-                    <a href="../webpages/create-test.html"><button class="azione">Crea Nuovo Test</button></a>
-                </div>
+                }
+            } catch (PDOException $e) {
+                echo ("Query Fallito") . $e->getMessage();
+                exit();
+            }
+            ?>
+        </div>
+        <h3 id="title">Azioni Disponibili</h3>
+        <div class="azioni">
+            <a href="../webpages/create-table-esercizio.html"><button class="azione">Crea Tabella Esercizio</button></a>
+            <a href="../webpages/create-test.html"><button class="azione">Crea Nuovo Test</button></a>
+        </div>
+        <h3 id="title">Tabelle Disponibili</h3>
+        <div class="tests">
+            <?php
+            try {
+                $sql = "SELECT * FROM ESERCIZI WHERE EmailDocente LIKE '$email'";
+                $result = $pdo->query($sql);
+
+                foreach ($result as $tabella) {
+                    $nome = $tabella['Nome'];
+                    $dataCreazioneTabella = $tabella['DataCreazione'];
+                    echo
+                    "<div class=\"test\">
+                                    <div id=\"info\">
+                                        <h3 style=\"color: var(--text);\">$nome</h3>
+                                        <h3 style=\"color: var(--text);\">$dataCreazioneTabella</h3>
+                                    </div>
+
+                                    <form action=\"./edit-tabella.php\" method=post>
+                                        <input type=\"hidden\" name=nome value=\"$nome\">
+                                        <button type=\"submit\">Edita</button>
+                                    </form>
+                                </div>";
+                }
+            } catch (PDOException $e) {
+                echo ("Fallito") . $e->getMessage();
+                exit();
+            }
+            ?>
+        </div>
     </div>
 </body>
